13f1f0674ac8c1e4de1f8ad805368786d4649a54
unknown <22411434+zodiuxus@users.noreply.github.com>
2025-01-23 13:38:13 +0100

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+# A repository containing all of my exam prep for a subject in uni
+
+Feel free to use whatever code you think will be valuable to you.
---
+++ patuvanja/vodic/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class VodicConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'vodic'
