9511514427fb80c9f103d05098e763e7aa22f370
unknown <22411434+zodiuxus@users.noreply.github.com>
2025-01-23 17:25:26 +0100

    Start basics

--- patuvanja/vodic/models.py
+++ patuvanja/vodic/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+#Patuvanja imaat mesto, vremetraenje i slika
+class Patuvanje(models.Model):
+    mesto = models.CharField(max_length = 255)
+    datum_pochetok = models.DateTimeField(auto_now=False, auto_now_add=False)
+    vremetraenje = models.IntegerField()
+    slika = models.ImageField(upload_to='data/')
+
+# Vodachi imaat ime, prezime, telefon, email
+# zaedno so relacija do patuvanja (m2m)
+class Vodach(models.Model):
+    ime = models.CharField(max_length=50)
+    prezime = models.CharField(max_length=50)
+    telefon = models.IntegerField()
+    mail = models.EmailField(max_length=254)
+    patuvanja_mtm = models.ManyToManyField(Patuvanje)
--- patuvanja/vodic/views.py
+++ patuvanja/vodic/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("If you're seeing this, then the index page is working, and likely everything else (for now).")
