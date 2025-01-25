ec1930d9ea72c78fa124db1847f781a69c2ea984
unknown <22411434+zodiuxus@users.noreply.github.com>
2025-01-25 17:14:10 +0100

    Complete backend

--- patuvanja/vodic/admin.py
+++ patuvanja/vodic/admin.py
@@ -27,9 +27,42 @@ class VodachAdmin(admin.ModelAdmin):
         
         vodachi = Vodach.objects.filter(patuvanja_mtm__isnull=False).filter(patuvanja_mtm__related_to=request.user).values_list('patuvanja_mtm', flat = True)
         return qs.filter(pk__in=vodachi)
-    
-    def has_view_permission(self, request, obj = None):
-        return True
+
+    def delete_model(self, request, obj):
+        pat_na_vod = obj.patuvanja_mtm.all()
+
+        ost_vodachi = (Vodach.objects
+                        .exclude(pk=obj.pk)
+                        .order_by('patuvanja_mtm')
+                        )
+
+        eligibility_flag = False
+        promeneti_vodachi = []
+        for vodach in ost_vodachi:
+            vod_cena_kap = 50000 - vodach.vkupna_cena
+            vod_pat_kap = 5 - vodach.patuvanja_mtm.count()
+            for patuvanje in pat_na_vod:
+                if patuvanje.cena <= vod_cena_kap and vod_pat_kap <= 5:
+                    if not eligibility_flag:
+                        eligibility_flag = True
+                    vodach.patuvanja_mtm.add(patuvanje)
+                    vod_cena_kap -= patuvanje.cena
+                    vod_pat_kap += 1
+                else:
+                    break
+
+            if eligibility_flag:
+                promeneti_vodachi.append(vodach)
+        
+        if not promeneti_vodachi:
+            self.message_user(request, "Ne postojat vodachi na koi mozhe da im se prenesat patuvanjata.")
+            return
+        
+        for vodach in promeneti_vodachi:
+            vodach.save()
+
+        super().delete_model(request, obj)
+
 
 admin.site.register(Patuvanje, PatuvanjeAdmin)
 admin.site.register(Vodach, VodachAdmin)
